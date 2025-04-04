19d29371abb4d6b69656540c069a6b315886db31
lc <lichaosz@qq.com>
2025-04-04 16:11:15 +0800

    delete python code

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Wind Ocean Energy AI'
+copyright = '2025, Li Chao Group'
+author = 'Li Chao'
 
 release = '0.1'
 version = '0.1.0'
